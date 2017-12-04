c9faf1afe32459f8ab66a669ddbe73e12b8b17a8
marjanmoro <34174217+marjanmoro@users.noreply.github.com>
2017-12-04 11:21:20 -0500

    Create myxterm.sh

---
+++ myxterm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Script that configures and launches xterm the way I like it.  System wide its referenced to this script to lauch xterm.
+
+xterm -fa monaco -fs 10 -bg white -fg black &
+
